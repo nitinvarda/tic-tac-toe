d2b9a53a4bf633428618f264d2d8892a81af24c8
Nitin <nitinvardha09@gmail.com>
2020-12-15 23:16:15 +0530

    errors resolved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Board from './Board'
 import './App.css';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-
+    // just checking
     <Router>
       <Switch>
         <Route path="/" component={Board} />
