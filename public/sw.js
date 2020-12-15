c541c4aad55c8c5e92415583e599277d2e166413
Nitin <nitinvardha09@gmail.com>
2020-12-15 21:44:17 +0530

    hosting test on vercel

--- public/sw.js
+++ public/sw.js
@@ -4,15 +4,15 @@ self.addEventListener("install", (event) => {
         caches.open(cacheData).then(cache => {
             console.log("adding Cache...")
             cache.addAll([
-                './static/js/main.chunk.js',
-                './static/js/0.chunk.js',
-                './static/js/bundle.js ',
+                '/static/js/main.chunk.js',
+                '/static/js/0.chunk.js',
+                '/static/js/bundle.js ',
                 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
                 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
                 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js',
                 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js',
-                './',
-                './index.html'
+                '/',
+                '/index.html'
             ])
         })
     )
