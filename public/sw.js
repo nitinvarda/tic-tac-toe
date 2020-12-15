d62473e6bdfced5137ecbf192aa4e914926fcf45
Nitin <nitinvardha09@gmail.com>
2020-12-15 22:26:40 +0530

    removed bootstrap files

--- public/sw.js
+++ public/sw.js
@@ -7,10 +7,6 @@ self.addEventListener("install", (event) => {
                 '/static/js/main.chunk.js',
                 '/static/js/0.chunk.js',
                 '/static/js/bundle.js ',
-                'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
-                'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
-                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js',
-                'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js',
                 '/',
                 '/home',
                 '/index.html'
