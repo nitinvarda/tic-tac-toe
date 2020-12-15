dd1b48b49b4fce76d978a4f2305591b92ec54481
Nitin <nitinvardha09@gmail.com>
2020-12-15 23:20:24 +0530

    added bootstrap links

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,11 @@ this.addEventListener("install", (event) => {
             return cache.addAll([
                 '/static/js/2.e2766815.chunk.js',
                 '/static/js/main.5a20eb76.chunk.js',
-                '/'
+                '/',
+                'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js',
+                'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+                'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
 
             ])
                 .catch(err => (
