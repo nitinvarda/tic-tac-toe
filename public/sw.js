b3f3b1613c89f6d60cceec28b0071ef315120ba3
Nitin <nitinvardha09@gmail.com>
2020-12-15 23:35:10 +0530

    changed location

--- public/sw.js
+++ public/sw.js
@@ -7,8 +7,8 @@ this.addEventListener("install", (event) => {
                 '/static/js/2.e2766815.chunk.js',
                 '/static/js/main.5a20eb76.chunk.js',
                 '/',
-                '/bootstrap.min.js',
-                '/bootstrap.min.css'
+                './bootstrap.min.js',
+                './bootstrap.min.css'
 
             ])
                 .catch(err => (
