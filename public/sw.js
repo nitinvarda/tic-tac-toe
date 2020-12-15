2244da6258a2ce072951797cccac267bcd7c203d
Nitin <nitinvardha09@gmail.com>
2020-12-15 23:23:42 +0530

    bootstrap min.js

--- public/sw.js
+++ public/sw.js
@@ -8,8 +8,6 @@ this.addEventListener("install", (event) => {
                 '/static/js/main.5a20eb76.chunk.js',
                 '/',
                 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js',
-                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js',
-                'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
                 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
 
             ])
