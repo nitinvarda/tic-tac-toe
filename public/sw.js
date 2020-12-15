b401134633fe3d7925ed1f7291d0c43b9ae329a4
Nitin <nitinvardha09@gmail.com>
2020-12-15 21:09:13 +0530

    changed source location

--- public/sw.js
+++ public/sw.js
@@ -2,16 +2,17 @@ const cacheData = 'appv1';
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(cacheData).then(cache => {
+            console.log("adding Cache...")
             cache.addAll([
-                '/static/js/main.chunk.js',
-                '/static/js/0.chunk.js',
-                '/static/js/bundle.js ',
+                './static/js/main.chunk.js',
+                './static/js/0.chunk.js',
+                './static/js/bundle.js ',
                 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
                 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
                 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js',
                 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js',
-                '/',
-                '/index.html'
+                './',
+                './index.html'
             ])
         })
     )
@@ -20,7 +21,7 @@ self.addEventListener("install", (event) => {
 
 // activated service worker
 self.addEventListener('activate', evt => {
-    // console.log('service worker has been activated')
+    console.log('service worker has been activated')
 
 })
 
@@ -28,7 +29,7 @@ self.addEventListener('activate', evt => {
 
 self.addEventListener('fetch', (event) => {
 
-
+    console.log('event triggered')
     if (!navigator.onLine) {
         event.respondWith(
             caches.match(event.request).then((res) => {
